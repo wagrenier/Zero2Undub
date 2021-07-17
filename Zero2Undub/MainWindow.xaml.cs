3ad08b67ec364b9497d85178f785663deee013c1
William Adam-Grenier <william.a-g@hotmail.com>
2021-07-17 17:21:31 -0400

    Working UI
    Still need to fix some things

--- Zero2Undub/MainWindow.xaml.cs
+++ Zero2Undub/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using Microsoft.Win32;
+using Zero2UndubProcess;
 
 namespace Zero2Undub
 {
@@ -9,17 +10,44 @@ namespace Zero2Undub
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string JpIsoFile { get; set; }
+        private string UsIsoFile { get; set; }
+        
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void LaunchUndubbing(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(JpIsoFile) && !string.IsNullOrWhiteSpace(UsIsoFile))
+            {
+                Zero2FileImporter.LaunchUndub(UsIsoFile, JpIsoFile);
+            }
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var openFileDialog = new OpenFileDialog();
-            if (openFileDialog.ShowDialog() == true)
+            var usFileDialog = new OpenFileDialog
             {
-                Console.WriteLine("Hi!");
+                Filter = "iso files (*.iso)|*.iso|All files (*.*)|*.*", 
+                Title = "Select the USA ISO"
+            };
+
+            if (usFileDialog.ShowDialog() == true)
+            {
+                UsIsoFile = usFileDialog.FileName;
+            }
+
+            var jpFileDialog = new OpenFileDialog
+            {
+                Filter = "iso files (*.iso)|*.iso|All files (*.*)|*.*", 
+                Title = "Select the USA ISO"
+            };
+
+            if (jpFileDialog.ShowDialog() == true)
+            {
+                JpIsoFile = jpFileDialog.FileName;
             }
         }
 
